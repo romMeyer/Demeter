6ec80256a552e5cef32572def6c6e22ee9a5cd62
Romain Meyer <romain.meyer.fkpt@gmail.com>
2024-09-19 10:03:26 +0200

    init Demeter

---
+++ src/main/java/fr/finkit/demeter/DemeterApplication.java
@@ -0,0 +1,13 @@
+package fr.finkit.demeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemeterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/finkit/demeter/DemeterApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.finkit.demeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
