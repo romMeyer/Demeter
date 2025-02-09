802749f219a5fecf2f4a80f71556b4612a1889aa
romMeyer <romain.meyer.fkpt@gmail.com>
2025-02-09 21:30:03 +0100

    suppression du componenent plants-lists

--- Client/src/app/shared/shared.module.ts
+++ Client/src/app/shared/shared.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatPaginatorModule } from '@angular/material/paginator'; 
 import { MatTableModule } from '@angular/material/table';
-import { PlantsListComponent } from './plants-list/plants-list.component';
 
 
 @NgModule({
   declarations: [
-    PlantsListComponent
   ],
   imports: [
     CommonModule,
