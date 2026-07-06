73070e9b4706401ae746935abdee297eabb73409
Romain Meyer <romain.meyer@eiffage;com>
2026-07-06 10:13:50 +0200

    Remove user

--- Server/src/main/resources/db/migration/V2025.11.27_16.49__ajout_user.sql
+++ Server/src/main/resources/db/migration/V2025.11.27_16.49__ajout_user.sql
@@ -1,3 +1,2 @@
 INSERT INTO users (first_name,last_name,username,role_id,"password") VALUES
-('admin','admin','admin@gmail.com',2,'$2a$10$EEanWAnqmtE7xu4FAlrBhuTvxovlR7Ax0LRuyo9lae7wFWG9OUdGW'),
-('Stévan','Camozz','test@gmail.com',1,'$2a$10$yp6fe5PMKWURR/ve6qj8ZuBGBEPAu7hVE0xlIMM6lulsi2CdRdxEK');
+('admin','admin','admin@gmail.com',2,'$2a$10$EEanWAnqmtE7xu4FAlrBhuTvxovlR7Ax0LRuyo9lae7wFWG9OUdGW');
